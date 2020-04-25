c6b6f6a937f8ed927fbea5a7e9b39a706ce1c67f
afmdanielwin <danielfarias380@gmail.com>
2020-04-25 09:35:53 -0300

    Setting production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
+const devMode = process.env.NODE_ENV !== 'production'
+
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: devMode? 'development' : 'production',
     entry: './src/main.js',
     output: {
-        filename: 'assets/js/main.js',
+        filename: devMode? 'assets/js/main.js' : 'assets/js/main.min.js' ,
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -21,6 +34,17 @@ module.exports = {
                 'css-loader', // Interpreta @import, url()...
                 'sass-loader'
             ]
-        }]
+        },
+        {
+            test: /.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }
+    ]
     }
 }
