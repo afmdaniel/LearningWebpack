9f2be305496e933abdbea65f2c8e757ce81771a0
afmdanielwin <danielfarias380@gmail.com>
2020-04-25 13:23:34 -0300

    Implementing webserver

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
         filename: devMode? 'assets/js/main.js' : 'assets/js/main.min.js' ,
         path: __dirname + '/public'
     },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
     optimization: {
         minimizer: [
             new UglifyJsPlugin({
