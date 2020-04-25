a6504560c033ac3ac628ddd4645c9436161d87bb
afmdanielwin <danielfarias380@gmail.com>
2020-04-25 13:23:53 -0300

    Just playing around

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import Person from './person'
-import './modules/moduleA'
 import './assets'
 
-const attendee = new Person;
-console.log(attendee.greet())
+document.querySelector("button").onclick = e => {
+    const black = "rgb(0, 0, 0)"
+    const white = "rgb(255, 255, 255)"
+    const body = document.querySelector('body')
+
+    body.style.backgroundColor = body.style.backgroundColor === black? white : black
+}
