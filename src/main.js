007c6491ad573697c72ef335d51b049e0f2d32cc
afmdanielwin <danielfarias380@gmail.com>
2020-04-25 14:00:28 -0300

    Adjust color

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import './assets'
 
 document.querySelector("button").onclick = e => {
     const black = "rgb(0, 0, 0)"
-    const white = "rgb(255, 255, 255)"
+    const green = "rgb(101, 221, 127)"
     const body = document.querySelector('body')
 
-    body.style.backgroundColor = body.style.backgroundColor === black? white : black
+    body.style.backgroundColor = body.style.backgroundColor === black? green : black
 }
